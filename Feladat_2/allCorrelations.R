4e19299d954d2fb0a524f5c3a2234973675af0cb
pietrsimon <55508152+pietrsimon@users.noreply.github.com>
2019-11-17 15:13:03 +0100

    Update allCorrelations.R

--- Feladat_2/allCorrelations.R
+++ Feladat_2/allCorrelations.R
@@ -1,3 +1,8 @@
+# Ez a függvény kiszámolja a tetszőleges window és lag értékek mellett
+# az összes eszköz közötti korrelációs idősorokat.
+
+# A keletkező mátrix kb 10MB-os, ezért ez nagyon lassan fut le.
+
 AllCorrelations = function(X, windowsize = 150, lagsize = 2){
   
   X$edges <- cbind( rep(2:length(X$LAST), each = (length(X$LAST)-1)), 
